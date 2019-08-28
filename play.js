1c4efd26d3919d0691dd3f26d5aa6a54d2a7d9d2
Anthony <anthonyzhu132@gmail.com>
2019-08-28 20:30:28 +0000

    refactored to call client.js file from module exports

--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-const net = require('net'); 
-// requiring net from library
-
-//establishes connection with game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.88.151',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  return conn;
-};
-
+const x = require('./client');
 console.log('connecting ...');
-connect();
+x();
